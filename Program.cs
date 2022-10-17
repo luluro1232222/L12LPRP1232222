37e38e60b0437056e6f68a4904a86078c5334859
Lucia <Lucia@DESKTOP-VLHNU2L>
2022-10-17 13:50:57 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] puntos = new int[14];
+
+        for (int i = 0; i < 14; i++)
+        {
+            Console.WriteLine("Ingrese nota ");
+            puntos[i] = Convert.ToInt16(Console.ReadLine());
+        }
+
+        double promedio = 0;
+        double suma = 0;
+        for (int i = 0; i < 14; i++)
+        {
+            suma = suma + puntos[i];
+        }
+
+        promedio = suma / 14;
+        Console.WriteLine("El promedio es " + promedio);
+    }
+}
